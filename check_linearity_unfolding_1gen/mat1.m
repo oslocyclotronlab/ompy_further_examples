9f6c9e8704e8b4bce2cabcf20c1ae5f7c9d2aad2
fzeiser <fzeiser@physnet.uni-hamburg.de>
2020-04-04 09:50:19 +0200

    Updated linearity notebook - unfolding is;fg not
    the unfolding seems to be approx linear when the number of iterations is chosen correctly
    
    for the fg method I found a better exampe, showing the non-linearity

---
+++ check_linearity_unfolding_1gen/mat1.m
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b41af62b22ab70626adb5e3b282d3dcea8540b8a44896d8cf7f92797e62fd6da
+size 568619
---
+++ check_linearity_unfolding_1gen/mat2.m
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:5a5330877ae773355de9db694b2bd0363ee773b8c920be45c21845677413eb80
+size 568619
